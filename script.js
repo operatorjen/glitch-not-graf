eb5226a78ff6390f1a199c60cf0fe7562ec06333
Glitch (hello-webpage) <none>
2019-01-27 16:38:33 +0000

    📯🎫 Checkpoint ./script.js:378534/1172 ./index.html:378534/437 ./style.css:378534/108

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+ctx.width = canvas.width = 500
+ctx.height = canvas.height = 500
+
+let x = ctx.width / 2
+let y = ctx.height / 2
+let switchX = false
+let switchY = false
+
+function render() {
+  ctx.clearRect(0, 0, ctx.width, ctx.height)
+  ctx.strokeStyle = 'rgba(10, 210, 100)'
+  x += Math.sin(x + Math.random() * 2) * Math.random() * 9
+  y += Math.cos(y + Math.random() * 2) * Math.random() * 9
+  
+  if (x > 
+  ctx.arc(x, y, 1, 0, 2 * Math.PI)
+  ctx.stroke()
+  window.requestAnimationFrame(render)
+}
+
+render()
